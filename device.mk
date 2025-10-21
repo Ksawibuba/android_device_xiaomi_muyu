ccd1c0d35815276af800fb54bf1a10343d11cedd
dodyirawan85 <dev.irawans@gmail.com>
2025-10-21 21:32:17 +0700

    muyu: Import power profile overlay
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- device.mk
+++ device.mk
@@ -19,6 +19,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# Overlays
+PRODUCT_PACKAGES += \
+    FrameworksResMuyu
+
 # Rootdir
 PRODUCT_PACKAGES += \
     init.muyu.rc
