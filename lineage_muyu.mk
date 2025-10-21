1964f63c7eb48431932075b6263450df06a1c05a
dodyirawan85 <dev.irawans@gmail.com>
2025-10-22 02:11:09 +0700

    muyu: Update blobs from muyu OS2.0.202.0.VOYMIXM
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- lineage_muyu.mk
+++ lineage_muyu.mk
@@ -23,5 +23,5 @@ PRODUCT_MODEL := 24091RPADG
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="missi-user 15 AQ3A.240801.002 OS2.0.200.0.VOYMIXM release-keys" \
-    BuildFingerprint=Xiaomi/muyu_global/muyu:14/UKQ1.240624.001/OS2.0.200.0.VOYMIXM:user/release-keys
+    BuildDesc="missi-user 15 AQ3A.240801.002 OS2.0.202.0.VOYMIXM release-keys" \
+    BuildFingerprint=Xiaomi/muyu_global/muyu:14/UKQ1.240624.001/OS2.0.202.0.VOYMIXM:user/release-keys
