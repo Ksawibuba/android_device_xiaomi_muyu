7942c1c36811e38cacbca74f0fcd33477ac017ca
dodyirawan85 <dev.irawans@gmail.com>
2025-12-19 11:17:56 +0700

    muyu: Update from OS3.0.4.0.WOYMIXM
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- lineage_muyu.mk
+++ lineage_muyu.mk
@@ -23,5 +23,5 @@ PRODUCT_MODEL := 24091RPADG
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="missi-user 16 BP2A.250605.031.A3 OS3.0.3.0.WOYMIXM release-keys" \
-    BuildFingerprint=Xiaomi/muyu_global/muyu:14/UKQ1.240624.001/OS3.0.3.0.WOYMIXM:user/release-keys
+    BuildDesc="missi-user 16 BP2A.250605.031.A3 OS3.0.4.0.WOYMIXM release-keys" \
+    BuildFingerprint=Xiaomi/muyu_global/muyu:14/UKQ1.240624.001/OS3.0.4.0.WOYMIXM:user/release-keys
