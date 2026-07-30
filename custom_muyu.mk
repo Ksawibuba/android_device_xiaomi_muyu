0c7cf14649259749f72dd286cd4e5c3886be7259
ksawi <ksawosigma@gmail.com>
2026-07-31 01:09:11 +0200

    muyu: rename lineage_muyu.mk to custom_muyu.mk

--- custom_muyu.mk
+++ custom_muyu.mk
@@ -9,14 +9,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/custom/config/common_full_tablet_wifionly.mk)
 
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/muyu/device.mk)
 
 PRODUCT_DEVICE := muyu
 PRODUCT_BRAND := Xiaomi
-PRODUCT_NAME := lineage_muyu
+PRODUCT_NAME := custom_muyu
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 24091RPADG
 
@@ -25,3 +25,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="missi-user 16 BP2A.250605.031.A3 OS3.0.7.0.WOYMIXM release-keys" \
     BuildFingerprint=Xiaomi/muyu_global/muyu:16/BP2A.250605.031.A3/OS3.0.7.0.WOYMIXM:user/release-keys
+
+-include vendor/lineage-priv/keys/keys.mk
